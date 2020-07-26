3af7c8d168f2b35bc2139a55f0d6b7a7d6fdd08b
Ramiz Rafiq <ramizrafiq465@gmail.com>
2020-07-26 11:06:59 -0700

    ready to commit

--- app.js
+++ app.js
@@ -9,42 +9,48 @@ var editBtn=document.createElement('button');
 var delText=document.createTextNode('Delete');
 var editText=document.createTextNode('Edit');
 var span=document.createElement('span');
+span.setAttribute("id","inputId")
 delBtn.appendChild(delText);
 editBtn.appendChild(editText);
-delBtn.setAttribute('class','btn');
-editBtn.setAttribute('class','btn');
-delBtn.classList.add('btn-Mr2');
-editBtn.classList.add('btn-Mr1');
+delBtn.setAttribute('class','btn2');
+editBtn.setAttribute('class','btn1');
+// delBtn.classList.add('btn-Mr2');
+// editBtn.classList.add('btn-Mr1');
 delBtn.setAttribute('onclick',"deleteli(this)");
 editBtn.setAttribute('onclick','edit(this)');
-
+var butSpan=document.createElement('span');
+butSpan.setAttribute("class","buttonsMr");
 
 if(inputValue.value==""){
 liText=document.createTextNode('Blank');
     li.appendChild(liText);
     li.appendChild(span);
-    li.appendChild(editBtn);
-    li.appendChild(delBtn);
-    
+    butSpan.appendChild(editBtn)
+    butSpan.appendChild(delBtn)
+    li.appendChild(butSpan);
+ 
     list.appendChild(li);
+
     
 }
 else{
     li.appendChild(liText);
     li.appendChild(span);
-    li.appendChild(editBtn);
-    li.appendChild(delBtn);
+    butSpan.appendChild(editBtn)
+    butSpan.appendChild(delBtn)
+    li.appendChild(butSpan);
+    
     
     list.appendChild(li);
     
-
+console.log(li)
 }
 inputValue.value="";
 
 }
 
 function deleteli(e){
-e.parentNode.remove();
+e.parentNode.parentElement.remove();
 }
 var cout=0;
 function edit(e){
@@ -52,18 +58,21 @@ function edit(e){
     inputTag.setAttribute('class','inputEdit');
     inputTag.setAttribute('id',"inputid");
     var inputid=document.getElementById('inputid');
+  e.parentNode.parentElement.children[0].appendChild(inputTag)
+  
 
-   
-    e.parentElement.children[0].appendChild(inputTag)
-    e.parentElement.children[1].innerHTML='Update';
+  e.parentNode.parentElement.children[1].children[0].innerHTML='Update'
     
    cout++;
-   console.log(cout);
+
    if(cout==2){
-    e.parentNode.childNodes[0].nodeValue=inputid.value;
-// console.log(inputid.value);
-    e.parentElement.children[0].innerHTML=" ";
-    e.parentElement.children[1].innerHTML='Edit';
+
+ e.parentNode.parentElement.childNodes[0].nodeValue=inputid.value
+
+e.parentNode.parentElement.children[0].innerHTML=" "
+e.parentNode.parentElement.children[1].children[0].innerHTML='Edit'
+ 
+
     cout=0;
    }
 
